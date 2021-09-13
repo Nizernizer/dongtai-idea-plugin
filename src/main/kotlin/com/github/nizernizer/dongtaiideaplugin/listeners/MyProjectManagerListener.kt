86e64940a4694f6ff4eb05bf0a48d5cb9d902541
GitHub Action <action@github.com>
2021-09-13 10:01:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/nizernizer/dongtaiideaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nizernizer/dongtaiideaplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nizernizer.dongtaiideaplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nizernizer.dongtaiideaplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/nizernizer/dongtaiideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nizernizer.dongtaiideaplugin.services
+
+import com.github.nizernizer.dongtaiideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
